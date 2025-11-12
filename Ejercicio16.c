e3438e02754a4e7918e733842dbdd30feb3b39dc
TonySchrader <schraderantonio@gmail.com>
2025-11-12 00:49:13 -0600

    Create Ejercicio16.c

---
+++ Ejercicio16.c
@@ -0,0 +1,29 @@
+/*Schrader Rodríguez Luis Antonio
+Tarea 2 Ejercicio 16*/
+
+#include <stdio.h>
+void main()
+{
+    int arrPar[100], i,numPar = 2;
+    
+    i = 0;
+
+    printf("Arreglo con los 100 primeros numeros pares");
+
+    for (i = 0; i < 100;i++)
+    {
+        arrPar[i] = numPar;
+        numPar += 2;
+    }
+    
+    printf("\nLos primeros 100 números pares son:\n");
+    for (i=0; i < 100; i++)
+    {
+        printf("%d ", arrPar[i]);
+
+        if ((i + 1) % 10 == 0)
+        {
+            printf("\n");
+        }
+    }
+}
