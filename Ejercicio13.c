aacc202677b31cd087032daf6763345242af73cf
TonySchrader <schraderantonio@gmail.com>
2025-11-12 00:03:18 -0600

    Create Ejercicio13.c

---
+++ Ejercicio13.c
@@ -0,0 +1,23 @@
+/*Schrader Rodríguez Luis Antonio
+Tarea 2 Ejercicio 13*/
+
+#include <stdio.h>
+
+void main()
+{
+    int arr[4];
+    int i;
+    int suma = 0;
+    
+     i = 0;
+
+    printf("Suma de enteros");
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("Dame el elemento %d del arreglo:", i+ 1);
+        scanf("%d",&arr[i]);
+        suma = suma + arr[i];
+    }
+    printf("\nLa suma total de los elementos del arreglo es: %d\n", suma);
+}
