8405660ef9ee44b45b91a2e01b64311ca761a7f5
TonySchrader <schraderantonio@gmail.com>
2025-11-12 10:50:15 -0600

    Update Ejercicio14.c

--- Ejercicio14.c
+++ Ejercicio14.c
@@ -11,11 +11,11 @@ void main()
     printf("Numero repetido en un arreglo\n");
     for (i = 0; i< 10; i++)
     {
-        printf("Ingrese el elemento %d del arreglo: ", i + 1);
+          printf("Dame el elemento %d del arreglo: ", i + 1);
         scanf("%d" ,&arr[i]);
     }
 
-    printf("\nIngrese el número que desea buscar en el arreglo: ");
+    printf("\nIngresa el número que quieres buscar en el arreglo: ");
     scanf("%d", &num);
 
     for (i = 0; i < 10; i++)
@@ -25,5 +25,5 @@ void main()
             cont++;
         }
     }
-    printf("\nEl número %d aparece %d veces en el arreglo.\n", num,cont);
+    printf("\nEl número %d se repite %d veces en el arreglo.\n", num,cont);
 }
