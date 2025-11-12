9b77c259227cd868d972aa4e3af6770eef54bc77
TonySchrader <schraderantonio@gmail.com>
2025-11-12 10:57:48 -0600

    Update Ejercicio15.c

--- Ejercicio15.c
+++ Ejercicio15.c
@@ -5,13 +5,13 @@ Tarea 2 Ejercicio 15*/
 
 void main()
 {
-    int arr[10], int i, posit = 0, negs = 0, nulos = 0;
+    int arr[10], i, posit = 0, negs = 0, nulos = 0;
     
     i=0;
 
     printf("Contar números positivos, negativos y nulos");
 
-    for (i = 0; i < 10; i++)
+    for (i =0; i < 10; i++)
     {
         printf("\nDame el número %d: ", i + 1);
         scanf("%d", &arr[i]);
@@ -34,5 +34,5 @@ void main()
     printf("\nConteo de numeros: \n");
     printf("Números positivos: %d\n", posit);
     printf("Números negativos: %d\n", negs);
-    printf("Números nulos: %d\n", nulos);
+    printf("Números nulos: %d\n", nulos );
 }
